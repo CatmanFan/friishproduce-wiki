88eaeef9bb882fd25b12813afddc1aa5d2a018c3
CatmanFan <oussama-studying@hotmail.com>
2024-12-28 14:53:43 +0100

    Update

--- build.bat
+++ build.bat
@@ -5,9 +5,9 @@ set /p txt=Message to commit:
 cls
 git commit -m "%txt%"
 git push -u origin main
+start bundle install
 cls
-echo Done. To test the site locally, please run the following commands:
-echo   bundle install
+echo Done. To test the site locally, please run the following command:
 echo   bundle exec jekyll serve
 echo.
 echo Press any key to exit.
