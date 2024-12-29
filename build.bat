b6a5c84d6cffeb1b52d15f6604a2762277e95236
CatmanFan <oussama-studying@hotmail.com>
2024-12-29 03:35:58 +0100

    Update main.scss

--- build.bat
+++ build.bat
@@ -1,20 +1,21 @@
 @echo off
 git add .
 cls
-set "txt=default"
-set /p "txt=Message to commit:"
+set txt=
+set /P "txt=Message to commit:"
 cls
-IF "%txt%"==default (
+
+set "TRUE="
+IF NOT DEFINED txt set TRUE=1
+IF "%txt%"=="" set TRUE=1
+IF defined TRUE (
     echo No commit message provided, will not push.
     goto :RUN
+) else (
+    git commit -m "%txt%"
+    git push -u origin main
 )
 
-git commit -m "%txt%"
-git push -u origin main
-
 :RUN
-start /wait bundle install
-bundle exec jekyll serve
 cls
-echo Done. Press any key to exit.
-pause >nul
+bundle install & bundle exec jekyll serve
