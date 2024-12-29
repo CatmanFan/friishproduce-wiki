44e48da85181077f3e3a3e869d0397590c697ecb
CatmanFan <oussama-studying@hotmail.com>
2024-12-29 03:24:32 +0100

    Test

--- build.bat
+++ build.bat
@@ -4,15 +4,16 @@ cls
 set "txt=default"
 set /p "txt=Message to commit:"
 cls
-IF "%txt%"=="" (
+IF "%txt%"==default (
     echo No commit message provided, will not push.
     goto :RUN
 )
+
 git commit -m "%txt%"
 git push -u origin main
 
 :RUN
-start /wait bundle install
+start /wait "bundle install"
 bundle exec jekyll serve
 cls
 echo Done. Press any key to exit.
